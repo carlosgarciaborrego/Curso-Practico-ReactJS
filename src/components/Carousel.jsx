5630ba322d98a535876a4c768494730d39b6817d
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2020-03-22 21:10:05 +0100

    Terminado el Curso Practico de React Js

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,21 +1,12 @@
-/* eslint-disable jsx-quotes */
-/* eslint-disable indent */
-/* eslint-disable semi */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable react/jsx-indent */
-/* eslint-disable react/jsx-closing-tag-location */
-/* eslint-disable react/self-closing-comp */
-
-import React from 'react';
+import React, { Children } from 'react';
 import '../assets/styles/components/Carousel.scss';
 
 const Carousel = ({ children }) => (
     <section className="carousel">
         <div className="carousel__container">
-            { children }
+            {children}
         </div>
-    </section>    
+    </section>
 );
 
 export default Carousel;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,64 +1,69 @@
-/* eslint-disable arrow-parens */
-/* eslint-disable jsx-quotes */
-/* eslint-disable indent */
+/* eslint-disable space-infix-ops */
 /* eslint-disable semi */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-closing-tag-location */
-/* eslint-disable react/self-closing-comp */
-// eslint-disable-next-line object-curly-spacing
+/* eslint-disable comma-dangle */
+/* eslint-disable react/jsx-curly-newline */
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable jsx-quotes */
+/* eslint-disable react/jsx-tag-spacing */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/jsx-indent */
 
-import React, { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => {
-  const [videos, setVideos] = useState([]);
-  useEffect(() => {
-    fetch(''API'')
-      .then(response => response.json())
-      .then(data => setVideos(data));
-  }, []);
+const API = 'http://localhost:3000/initialState';
 
-  return (
-  <div className="App">
-         <Header />
-         <Search />
-         <Categories title="Mi lista">
-           <Carousel>
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-           </Carousel>
-         </Categories>
-         <Categories title="Tendencias">
-           <Carousel>
-              <CarouselItem />
-              <CarouselItem />
-           </Carousel>
-         </Categories>
-         <Categories title="Originales de Platzi Video">
-           <Carousel>
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-              <CarouselItem />
-           </Carousel>
-         </Categories>
-         <Footer />
- </div>
-);
+const App = () => {
+    const initialState = useInitialState(API);
+
+    return initialState.length === 0 ? <h1>Loading...</h1>: (
+     <div className="App">
+        <Header/>
+        <Search/>
+        
+        {initialState.mylist.length > 0 &&
+            <Categories title="Mi Lista">
+                <Carousel>
+                {initialState.mylist.map(item =>
+                    <CarouselItem key={item.id} {...item}/>
+                )}
+                </Carousel>
+            </Categories>
+        }
+
+        <Categories title="Tendencias">
+            <Carousel>
+                {initialState.trends.map(item =>
+                    <CarouselItem key={item.id} {...item}/>
+                )}
+            </Carousel>
+        </Categories>
+
+        <Categories title="Originales de PlatziVideo">
+            <Carousel>
+                {initialState.originals.map(item =>
+                    <CarouselItem key={item.id} {...item}/>
+                )}
+            </Carousel>
+        </Categories>    
+        <Footer/>
+    </div>
+    );
 }
 
 export default App;
